78a870a96f8033180505b2815dcb1842e790fa33
Aldi (mbp) <you@example.com>
2025-05-30 11:51:41 +0200

    Dataset update and vis

--- python/pascal_voc_dataset/pascal_voc_dataset.py
+++ python/pascal_voc_dataset/pascal_voc_dataset.py
@@ -6,32 +6,73 @@ import torchvision.transforms as transforms
 from pascal_voc_dataset.download_pascal_voc_dataset import download_pascal_voc_2012_dataset
 from PIL import Image
 from torch.utils.data import Dataset
+import numpy as np
+import torch
 
+VOC_COLORMAP = np.array(
+    [
+        [0, 0, 0],  # 0=background
+        [128, 0, 0],  # 1=aeroplane
+        [0, 128, 0],  # 2=bicycle
+        [128, 128, 0],  # 3=bird
+        [0, 0, 128],  # 4=boat
+        [128, 0, 128],  # 5=bottle
+        [0, 128, 128],  # 6=bus
+        [128, 128, 128],  # 7=car
+        [64, 0, 0],  # 8=cat
+        [192, 0, 0],  # 9=chair
+        [64, 128, 0],  # 10=cow
+        [192, 128, 0],  # 11=diningtable
+        [64, 0, 128],  # 12=dog
+        [192, 0, 128],  # 13=horse
+        [64, 128, 128],  # 14=motorbike
+        [192, 128, 128],  # 15=person
+        [0, 64, 0],  # 16=potted plant
+        [128, 64, 0],  # 17=sheep
+        [0, 192, 0],  # 18=sofa
+        [128, 192, 0],  # 19=train
+        [0, 64, 128],  # 20=tv/monitor
+    ],
+    dtype=np.uint8,
+)
+
+
+def decode_voc_mask(mask: torch.Tensor) -> np.ndarray:
+    if isinstance(mask, torch.Tensor):
+        mask = mask.detach().cpu().numpy()
+    mask = mask.astype(np.uint8)
+
+    h, w = mask.shape
+    rgb_mask = np.zeros((h, w, 3), dtype=np.uint8)
+
+    valid = mask != 255
+    rgb_mask[valid] = VOC_COLORMAP[mask[valid]]
+    return rgb_mask
 
 class ViTV16Transforms:
-    def __init__(self, size=(256, 256)):
+
+    def __init__(self):
         """
         Matching transforms for: https://docs.pytorch.org/vision/main/models/generated/torchvision.models.vit_b_16.html#torchvision.models.ViT_B_16_Weights
         """
-        self.image_transform = transforms.Compose(
-            [
-                T.Resize(256, interpolation=transforms.InterpolationMode.BILINEAR),
-                T.CenterCrop(224),
-                T.ToTensor(),
-                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ]
-        )
-        self.mask_transform = T.Compose(
-            [
-                T.Resize(256, interpolation=transforms.InterpolationMode.BILINEAR),
-                T.CenterCrop(224),
-                T.ToTensor(),
-            ]
-        )
+        self.image_transform = T.Compose([
+            T.Resize(256, interpolation=T.InterpolationMode.BILINEAR),
+            T.CenterCrop(224),
+            T.ToTensor(),
+            # T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        self.mask_resize = T.Resize(256, interpolation=T.InterpolationMode.NEAREST)
+        self.mask_crop = T.CenterCrop(224)
 
-    def __call__(self, image, mask):
-        return self.image_transform(image), self.mask_transform(mask).long().squeeze(0)
+    def __call__(self, image: Image.Image, mask: Image.Image):
+        image = self.image_transform(image)
 
+        mask = np.array(mask, dtype=np.uint8)
+        mask = Image.fromarray(mask)
+        mask = self.mask_resize(mask)
+        mask = self.mask_crop(mask)
+        mask = torch.from_numpy(np.array(mask)).long()  
+        return image, mask
 
 class PascalVOCDataset(Dataset):
     def __init__(self, root_dir, split="train"):
@@ -66,6 +107,8 @@ class PascalVOCDataset(Dataset):
 
         image = Image.open(image_path).convert("RGB")
         mask = Image.open(mask_path)
+        mask = np.array(mask, dtype=np.uint8)
+        mask = torch.from_numpy(mask).long()
 
         if self.transforms:
             image, mask = self.transforms(image, mask)
--- python/utils/misc.py
+++ python/utils/misc.py
@@ -6,7 +6,8 @@ import numpy as np
 import torch
 import yaml
 from PIL import Image
-
+import matplotlib.pyplot as plt
+from pascal_voc_dataset.pascal_voc_dataset import decode_voc_mask
 
 def get_device():
     device = torch.device(
@@ -22,7 +23,7 @@ def load_config(path):
 
 def get_logger(log_dir):
     os.makedirs(log_dir, exist_ok=True)
-    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+    now = datetime.now().strftime("%Y%m%d_%H%M%S") 
     log_file = os.path.join(log_dir, f"log_{now}.log")
     logger = logging.getLogger(f"logger_{now}")
     logger.setLevel(logging.INFO)
@@ -46,3 +47,28 @@ def plot_single_image(tensor, filename):
     img = np.transpose(img, (1, 2, 0))
     img = Image.fromarray((img * 255).astype(np.uint8))
     img.save(filename)
+
+
+
+def save_image_with_mask(image, mask, filename, alpha=0.5):
+    assert image.ndim == 3 and image.shape[0] == 3, "Image must be of shape (3, n, h)"
+    assert mask.ndim == 2 and mask.shape == image.shape[1:], "Mask must be of shape (n, h)"
+
+    image_np = image.detach().cpu().float().numpy()
+    if image_np.max() > 1.0:
+        image_np = image_np / 255.0
+    image_np = np.transpose(image_np, (1, 2, 0))  # (H, W, 3)
+
+    # Convert mask to RGB
+    mask_rgb = decode_voc_mask(mask) / 255.0  # (H, W, 3), float in [0, 1]
+
+    # Create overlay
+    overlay = image_np * (1 - alpha) + mask_rgb * alpha
+    overlay = np.clip(overlay, 0, 1)
+
+    # Concatenate horizontally
+    panel = np.concatenate([image_np, mask_rgb, overlay], axis=1)
+
+    # Save to file
+    plt.imsave(filename, panel)
+
